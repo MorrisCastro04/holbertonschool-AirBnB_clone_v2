7d16161edc5ae666e284b612cce544648e5b9cb3
MorrisCastro <jomarcastro2004@gmail.com>
2024-04-11 17:34:25 -0400

    fix pycode

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+"""starts a Flask web aplication"""
 from flask import Flask
-
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
@@ -10,6 +11,7 @@ def hello_hbnb():
     It returns a string 'Hello HBNB!' when the route is accessed.
     """
     return 'Hello HBNB!'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
