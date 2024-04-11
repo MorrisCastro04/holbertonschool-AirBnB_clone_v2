1685cd52fca9a47345581674dc19589d44a37e21
MorrisCastro <jomarcastro2004@gmail.com>
2024-04-11 17:38:40 -0400

    fix pycode style task1

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,15 +1,24 @@
 #!/usr/bin/python3
+"""starts Flask web aplication"""
 from flask import Flask
-
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """
+    This function handles the root route and returns a greeting message.
+    """
     return 'Hello HBNB!'
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """
+    This function is a route handler for the '/hbnb' endpoint.
+    """
     return 'HBNB'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
