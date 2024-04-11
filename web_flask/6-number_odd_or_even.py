670f43c02fef09d0e98b834d8d386cfc4d8926dd
MorrisCastro <jomarcastro2004@gmail.com>
2024-04-11 18:00:58 -0400

    pycodestyle task 6

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -1,45 +1,70 @@
 #!/usr/bin/python3
+"""starts flask web aplication"""
 from flask import Flask, render_template
-
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """
+    Returns a greeting message.
+    """
     return 'Hello HBNB!'
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """
+    Returns the string 'HBNB' when the '/hbnb' route is accessed.
+    """
     return 'HBNB'
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def text(text):
+    """
+    Converts the given text parameter by replacing
+    underscores with spaces and returns it with a prefix 'C'.
+    """
     text = text.replace("_", " ")
     return "C {}".format(text)
+
 
 @app.route('/python', defaults={'text': "is cool"}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def py(text):
+    """
+    This function handles the '/python' route and the '/python/<text>' route.
+    """
     text = text.replace('_', ' ')
     return "Python {}".format(text)
 
-@app.route('/number/<n>', strict_slashes=False)
+
+@app.route('/number/<int:n>', strict_slashes=False)
 def numbers(n):
-    if n.isdigit():
-        return '{} is a number'.format(n)
+    """
+    This function checks if the provided value is a
+    number and returns a message accordingly.
+    """
+    return '{} is a number'.format(n)
 
-@app.route('/number_template/<n>', strict_slashes=False)
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
 def temp(n):
-    if n.isdigit():
-        return render_template('5-number.html', n=n)
+    """
+    Renders the '5-number.html' template with the given number.
+    """
+    return render_template('5-number.html', n=n)
 
-@app.route('/number_odd_or_even/<n>', strict_slashes=False)
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def even_odd(n):
-    if n.isdigit():
-        if int(n) % 2 == 0:
-            EvenOdd = "even"
-        else:
-            EvenOdd = "odd"
-        return render_template('6-number_odd_or_even.html', n=n, EvenOdd=EvenOdd)
+    if int(n) % 2 == 0:
+        EvenOdd = "even"
+    else:
+        EvenOdd = "odd"
+    return render_template('6-number_odd_or_even.html', n=n, EvenOdd=EvenOdd)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
