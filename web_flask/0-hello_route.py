fa991e0892cb63fb38182f0d6c7ad4a3248adfb6
MorrisCastro <jomarcastro2004@gmail.com>
2024-04-11 17:28:56 -0400

    doc task 0 and add method to dbStorage

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """
+    This function handles the root route of the Flask application.
+    It returns a string 'Hello HBNB!' when the route is accessed.
+    """
     return 'Hello HBNB!'
 
 if __name__ == '__main__':
