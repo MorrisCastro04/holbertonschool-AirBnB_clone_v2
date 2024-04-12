ffaab1644da9089ce9910a8dcee15161e16dbae0
MorrisCastro <jomarcastro2004@gmail.com>
2024-04-11 23:53:36 -0400

    add documentacion

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""starts flask web application"""
 from flask import Flask, render_template
 from models import storage
 from models.state import State
@@ -7,11 +8,18 @@ app = Flask(__name__)
 
 @app.teardown_request
 def teardown(self):
+    """
+    This function is a teardown function that is executed after each request.
+    It closes the storage connection.
+    """
     storage.close()
 
 
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_states():
+    """
+    Renders a template that displays a list of cities grouped by states.
+    """
     states = sorted(storage.all(State).values(), key=lambda state: state.name)
     return render_template('8-cities_by_states.html', states=states)
 
